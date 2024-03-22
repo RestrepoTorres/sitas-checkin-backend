4f163afb8703d57e1312da53c991b4cf0af96190
DavidTorres-sys <david.torresg@udea.edu.co>
2024-03-21 20:57:54 -0500

    :heavy_plus_sign: Swagger dependency add to the pom
    Swagger dependency add to the pom.xml

--- src/main/java/com/sitas/checkin/config/SwaggerConfig.java
+++ src/main/java/com/sitas/checkin/config/SwaggerConfig.java
@@ -1,5 +1,20 @@
 package com.sitas.checkin.config;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
 public class SwaggerConfig {
 
+    @Bean
+    public OpenAPI api() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Backend checkIn")
+                .version("1.0.0")
+                .description("Endpoints for the backend of CheckIn of the SITAS project"));
+    }
 }
